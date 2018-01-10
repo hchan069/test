ec6ee623589a1ebf0eadf3d16438a7a334ecc701
Ho Yee Chan <hchan069@hammer.cs.ucr.edu>
2018-01-10 09:19:46 -0800

    added initial main

--- main.cpp
+++ main.cpp
@@ -2,13 +2,6 @@
 
 int main()
 {
-<<<<<<< HEAD
-=======
-	std::string name;
-	std::cout << "What is your name?" << std::endl;
-	std::cin >> name;
-	std::cout << "hello" << name << "!" << std::endl;
-
-	return 0;
->>>>>>> userinput
+	std::cout << "hello git!" << std:endl;
+	return 1;
 }
